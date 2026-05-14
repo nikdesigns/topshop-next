aefe41fd7655dd5436159d97b827a2e3f43d5558
Nitin <nitinkaushikdesigns@gmail.com>
2026-05-14 14:27:14 +0530

    18. images update

---
+++ data/topshop_2026_company_profiles.ts
@@ -0,0 +1,315 @@
+export type TopShop2026CompanyProfile = {
+  company: string;
+  aliases?: string[];
+  image: string;
+  address: string;
+  phone: string;
+};
+
+export const TOPSHOP_2026_COMPANY_PROFILE_FALLBACK = {
+  image: '/assets/images/logo/ts_winner_logo.png',
+  address: 'Contact The145 for official winner contact details.',
+  phone: 'N/A',
+} as const;
+
+/*
+  Single editable source for Top Shop 2026 winner company media + contact details.
+  Update image/address/phone here when details change.
+*/
+export const TOPSHOP_2026_COMPANY_PROFILES: TopShop2026CompanyProfile[] = [
+  {
+    company: 'A&R Aviation Services',
+    image: '/assets/images/clients/ar_logo.svg',
+    address: TOPSHOP_2026_COMPANY_PROFILE_FALLBACK.address,
+    phone: TOPSHOP_2026_COMPANY_PROFILE_FALLBACK.phone,
+  },
+  {
+    company: 'A.I.R.S.',
+    image: '/assets/images/ts_winner_2026/airs.webp',
+    address: '3905 Newpoint Dr, Hilliard, OH 43026',
+    phone: '+1 614-876-7000',
+  },
+  {
+    company: 'AAR Component Services - Grand Prairie',
+    image: '/assets/images/ts_winner_2026/aar.webp',
+    address: '1038 Santerre St, Grand Prairie, TX 75050',
+    phone: '+1 972-623-9300',
+  },
+  {
+    company: 'AAR Corporation',
+    image: '/assets/images/ts_winner_2026/aar.webp',
+    address: '1100 N Wood Dale Rd, Wood Dale, IL 60191',
+    phone: '+1 630-227-2000',
+  },
+  {
+    company: 'Aero Accessories, Inc',
+    image: '/assets/images/ts_winner_2026/aeroacc.webp',
+    address: '2110 N Marshall Ave, El Cajon, CA 92020',
+    phone: '+1 619-448-2558',
+  },
+  {
+    company: 'Aero Instruments & Avionics',
+    image: '/assets/images/ts_winner_2026/aeroacc.webp',
+    address: '3332 Walden Ave, Depew, NY 14043',
+    phone: '+1 716-694-7060',
+  },
+  {
+    company: 'AeroParts Manufacturing & Repair, Inc',
+    image: '/assets/images/ts_winner_2026/aero_parts.webp',
+    address: '431 Rio Rancho Blvd NE, Rio Rancho, NM 87124',
+    phone: '+1 505-891-6600',
+  },
+  {
+    company: 'Aerostar, Inc',
+    image: '/assets/images/ts_winner_2026/aerostar.webp',
+    address: '2150 Michigan Avenue Mobile, AL 36615',
+    phone: '251.650.0927',
+  },
+  {
+    company: 'AerSale Component Solutions',
+    image: '/assets/images/ts_winner_2026/aersale.webp',
+    address: '7600 NW 26th St, Miami, FL 33122',
+    phone: '+1 305-593-6038',
+  },
+  {
+    company: 'Ametek MRO - Drake Air Tulsa',
+    image: '/assets/images/ts_winner_2026/amtek.webp',
+    address: '4085 Southwest Blvd, Tulsa, OK 74107',
+    phone: '+1 918-445-3545',
+  },
+  {
+    company: 'AllClear Repair Services',
+    image: '/assets/images/ts_winner_2026/allclear.webp',
+    address: TOPSHOP_2026_COMPANY_PROFILE_FALLBACK.address,
+    phone: TOPSHOP_2026_COMPANY_PROFILE_FALLBACK.phone,
+  },
+  {
+    company: 'Allflight Corporation',
+    image: '/assets/images/ts_winner_2026/allflight.webp',
+    address: '20014 70th Ave S, Kent, WA 98032',
+    phone: '+1 253-437-0582',
+  },
+  {
+    company: 'AOG Reaction, Inc',
+    image: '/assets/images/ts_winner_2026/aog.webp',
+    address: '526 Aviator Dr #5433, Fort Worth, TX 76179',
+    phone: '+1 817-439-0700',
+  },
+  {
+    company: 'ATS Components DFW',
+    image: '/assets/images/ts_winner_2026/ats.webp',
+    address: '15050 Trinity Blvd, Fort Worth, TX 76155',
+    phone: '+1 972-709-2866',
+  },
+  {
+    company: 'AvDUCT Worldwide',
+    image: '/assets/images/ts_winner_2026/avduct.webp',
+    address: '1630 N. 166th East Ave. Tulsa, OK 74116',
+    phone: '(918) 437-7772',
+  },
+  {
+    company: 'Aviation Inflatables',
+    image: '/assets/images/ts_winner_2026/aviation_inf.webp',
+    address: TOPSHOP_2026_COMPANY_PROFILE_FALLBACK.address,
+    phone: TOPSHOP_2026_COMPANY_PROFILE_FALLBACK.phone,
+  },
+  {
+    company: 'Aviation Repair Technologies',
+    image: '/assets/images/ts_winner_2026/art.webp',
+    address: TOPSHOP_2026_COMPANY_PROFILE_FALLBACK.address,
+    phone: TOPSHOP_2026_COMPANY_PROFILE_FALLBACK.phone,
+  },
+  {
+    company: 'AvGen Aerospace',
+    image: '/assets/images/ts_winner_2026/avgen.webp',
+    address: '7525 W 19th Ct, Hialeah, FL 33014',
+    phone: '+1 786-534-3727',
+  },
+  {
+    company: 'B&W Aviation Corp',
+    image: '/assets/images/ts_winner_2026/bw.webp',
+    address: '7317 NW 56th Street Miami, FL',
+    phone: '(305) 884-7303',
+  },
+  {
+    company: 'BP Aero Services',
+    image: '/assets/images/ts_winner_2026/bpaero.webp',
+    address: '4961 Hanson Dr, Irving, TX 75038',
+    phone: '972-252-2800',
+  },
+  {
+    company: 'Cima Aviation',
+    image: '/assets/images/ts_winner_2026/cima.webp',
+    address: '2200 Northwest 84 Ave, Doral, Florida 33122',
+    phone: '+1 786-391-1315',
+  },
+  {
+    company: 'Cobalt Aero Services',
+    image: '/assets/images/ts_winner_2026/cobalt.webp',
+    address: TOPSHOP_2026_COMPANY_PROFILE_FALLBACK.address,
+    phone: TOPSHOP_2026_COMPANY_PROFILE_FALLBACK.phone,
+  },
+  {
+    company: 'Curtiss-Wright Surface Technologies',
+    image: '/assets/images/ts_winner_2026/curt.webp',
+    address: TOPSHOP_2026_COMPANY_PROFILE_FALLBACK.address,
+    phone: TOPSHOP_2026_COMPANY_PROFILE_FALLBACK.phone,
+  },
+  {
+    company: 'CVG Aerospace',
+    image: '/assets/images/ts_winner_2026/cvg.webp',
+    address: '13500 SW 134th Ave, Miami, FL 33186',
+    phone: '+1 786-293-9923',
+  },
+  {
+    company: 'EMC Aerospace, Inc',
+    image: '/assets/images/ts_winner_2026/emc.webp',
+    address: '570 NE 185th St, North Miami Beach, FL 33179',
+    phone: '+1 954-316-6015',
+  },
+  {
+    company: 'Evans Composites, Inc',
+    image: '/assets/images/ts_winner_2026/evans.webp',
+    address: '300 S Wisteria St, Mansfield, TX 76063',
+    phone: '+1 817-477-9014',
+  },
+  {
+    company: 'Fokker Services Group',
+    image: '/assets/images/ts_winner_2026/fokker.webp',
+    address: 'Hoeksteen 40, 2132 MS, Hoofddorp, Netherlands',
+    phone: '+31 (0)88 6280 000',
+  },
+  {
+    company: 'GA Telesis Landing Gear Services, LLC',
+    image: '/assets/images/ts_winner_2026/ga.webp',
+    address: '1855 Griffin Rd, Dania Beach, FL 33004',
+    phone: '+1 954-676-3111',
+  },
+  {
+    company: 'HEICO Repair Group - Aerostructures',
+    image: '/assets/images/ts_winner_2026/heico.webp',
+    address: '7875 NW 64th St, Miami, FL 33166',
+    phone: '+1 305-463-0455',
+  },
+  {
+    company: 'HRD Aero Systems, Inc',
+    image: '/assets/images/ts_winner_2026/hrd.webp',
+    address: '25555 Avenue Stanford, Valencia, CA 91355',
+    phone: '+1 661-407-2772',
+  },
+  {
+    company: 'Icon Aerospace, LLC',
+    image: '/assets/images/ts_winner_2026/icon.webp',
+    address: '600 Radiator Rd, Indian Trail, NC 28079',
+    phone: '+1 704-821-4266',
+  },
+  {
+    company: 'Iliff Aircraft - ATA 38',
+    image: '/assets/images/ts_winner_2026/iliff.webp',
+    address: '3225 N Sheridan Rd #17, Tulsa, OK 74115',
+    phone: '+1 918-835-5554',
+  },
+  {
+    company: 'Innodyne Systems, LLC',
+    image: '/assets/images/ts_winner_2026/inno.webp',
+    address: '109 W Dudley Town Rd D, Bloomfield, CT 06002',
+    phone: '+1 860-242-2442',
+  },
+  {
+    company: 'JJA Aviation',
+    image: '/assets/images/ts_winner_2026/jja.webp',
+    address: '14450 Commerce Way, Miami Lakes, FL 33016',
+    phone: '+1 305-498-7920',
+  },
+  {
+    company: 'Lift MRO',
+    image: '/assets/images/ts_winner_2026/lift.webp',
+    address: '5475 NW 72nd Ave, Miami, FL 33166',
+    phone: '+1 305-574-9932',
+  },
+  {
+    company: 'MARTEC Aviation',
+    image: '/assets/images/ts_winner_2026/martec.webp',
+    address: '3980 W 104th St Suite 1, Hialeah, FL 33018',
+    phone: '+1 305-456-7563',
+  },
+  {
+    company: 'MTI Aviation',
+    image: '/assets/images/ts_winner_2026/mti.webp',
+    address: '13150 NW 45th Avenue, Miami, FL 33054',
+    phone: '305-817-4244',
+  },
+  {
+    company: 'Next MRO',
+    image: '/assets/images/ts_winner_2026/next.webp',
+    address: '15800 NW 49th Ave, Miami Gardens, FL 33014',
+    phone: '+1 305-230-4305',
+  },
+  {
+    company: 'North Bay Aviation',
+    image: '/assets/images/ts_winner_2026/north.webp',
+    address: '424 Executive Ct N STE E, Fairfield, CA 94534',
+    phone: '+1 707-863-4970',
+  },
+  {
+    company: 'Setnix LLC',
+    image: '/assets/images/ts_winner_2026/setnix.webp',
+    address: '475 Bond St, Lincolnshire, IL 60069',
+    phone: '+1 312-549-4459',
+  },
+  {
+    company: 'Silver Wings Aerospace',
+    image: '/assets/images/ts_winner_2026/silver.webp',
+    address: '25400 SW 140th Ave, Princeton, FL 33032',
+    phone: '+1 305-258-5950',
+  },
+  {
+    company: 'Soundair Aviation Svcs',
+    image: '/assets/images/ts_winner_2026/soundair.webp',
+    address: '1826 Bickford Ave, Snohomish, WA 98290',
+    phone: '(360) 453-2300',
+  },
+  {
+    company: 'StandardAero',
+    image: '/assets/images/ts_winner_2026/standard.webp',
+    address: '11550 Mosteller Rd, Cincinnati, OH 45241',
+    phone: '+1 513-618-9588',
+  },
+  {
+    company: 'Summit Aerospace, Inc',
+    aliases: ['Summit Aerospace, Inc.'],
+    image: '/assets/images/ts_winner_2026/summit.webp',
+    address: '8130 NW 74th Ave, Medley, FL 33166',
+    phone: '+1 305-267-6400',
+  },
+  {
+    company: 'TAT Technologies - Greensboro',
+    image: '/assets/images/ts_winner_2026/tat.webp',
+    address: '7102 Cessna Drive Greensboro, NC 27409',
+    phone: 'N/A',
+  },
+  {
+    company: 'TAG Aero, LLC',
+    image: '/assets/images/ts_winner_2026/tag.webp',
+    address: '1247 Apex Dr, Rock Hill, SC 29730',
+    phone: '+1 803-831-9390',
+  },
+  {
+    company: 'Unicorp Systems, Inc',
+    image: '/assets/images/ts_winner_2026/unicorp.webp',
+    address: TOPSHOP_2026_COMPANY_PROFILE_FALLBACK.address,
+    phone: TOPSHOP_2026_COMPANY_PROFILE_FALLBACK.phone,
+  },
+  {
+    company: 'VSE Aviation Services - FL',
+    image: '/assets/images/ts_winner_2026/vse.webp',
+    address: '570 NE 185th St, North Miami Beach, FL 33179',
+    phone: '+1 954-316-6015',
+  },
+  {
+    company: 'VSE Aviation Services - KY',
+    image: '/assets/images/ts_winner_2026/vse.webp',
+    address: '3000 Kustom Dr, Hebron, KY 41048',
+    phone: '+1 859-283-2264',
+  },
+];
